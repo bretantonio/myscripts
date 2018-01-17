7a3faab3de2f7fe9e02a73bf6644637a5bf16a58
Bret Joseph Antonio <bretantonio@users.noreply.github.com>
2018-01-17 10:35:32 +0200

    Added Sites Support and Removed Sound
    Support for Lite Coin and Ethereum

--- coinminer.js
+++ coinminer.js
@@ -2,11 +2,13 @@
 // @name         Coin Miner autosave
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  auto mine coins from coin miner
+// @description  auto save coins
 // @author       Bret Joseph
-// @match        http://coinminer.mobi/terminal
+// @match        http*://coinminer.mobi/terminal
+// @match        http*://ltcminer.pw/terminal
+// @match        http*://ethminer.cash/terminal
 // @grant        none
 // ==/UserScript==
 
-setInterval(function(){
-    _TerminalSave();}, 60000);
+setInterval(function(){_TerminalSave();}, 60000);
+_TerminalSound();
