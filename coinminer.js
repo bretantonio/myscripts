0b77b7581684445792a27ec172fc3dca5a8a4520
bret <bretjoseph@gmail.com>
2018-01-17 16:28:36 +0200

    Update

--- coinminer.js
+++ coinminer.js
@@ -1,12 +1,17 @@
 // ==UserScript==
 // @name         Coin Miner autosave
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.1.1
 // @description  auto save coins
 // @author       Bret Joseph
-// @match        http*://coinminer.mobi/terminal
-// @match        http*://ltcminer.pw/terminal
-// @match        http*://ethminer.cash/terminal
+// @homepageURL  https://raw.githubusercontent.com/bretantonio/myscripts/master/coinminer.js
+// @updateURL    https://raw.githubusercontent.com/bretantonio/myscripts/master/coinminer.js
+// @match        https://coinminer.mobi/terminal
+// @match        http://coinminer.mobi/terminal
+// @match        https://ltcminer.pw/terminal
+// @match        http://ltcminer.pw/terminal
+// @match        https://ethminer.cash/terminal
+// @match        http://ethminer.cash/terminal
 // @grant        none
 // ==/UserScript==
 
