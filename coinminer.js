2cc520a95b4b960caab88e39897ccd6e8ce5cf4f
Bret Joseph Antonio <bretantonio@users.noreply.github.com>
2018-04-14 11:57:43 +0200

    Global Update
    changed for the whole site

--- coinminer.js
+++ coinminer.js
@@ -1,19 +1,14 @@
 // ==UserScript==
-// @name         Coin Miner autosave
+// @name         Auto CPUCAP
 // @namespace    http://tampermonkey.net/
-// @version      0.1.2
+// @version      0.1.3
 // @description  auto save coins
 // @author       Bret Joseph
 // @homepageURL  https://github.com/bretantonio/myscripts/blob/master/coinminer.js
 // @updateURL    https://raw.githubusercontent.com/bretantonio/myscripts/master/coinminer.js
-// @match        https://bitcoin.cpucap.com/terminal
-// @match        http://bitcoin.cpucap.com/terminal
-// @match        https://litecoin.cpucap.com/terminal
-// @match        http://litecoin.cpucap.com/terminal
-// @match        https://ethereum.cpucap.com/terminal
-// @match        http://ethereum.cpucap.com/terminal
+// @match        http*://*cpucap.com/terminal
 // @grant        none
 // ==/UserScript==
 
-setInterval(function(){_TerminalSave();}, 60000);
+setInterval(function(){_TerminalUpdate();}, 60000);
 _TerminalSound();
